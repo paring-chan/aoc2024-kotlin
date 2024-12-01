a52db2c073eeb4447b1384ee7274f07ded62c76b
paring <paringchan@proton.me>
2024-12-01 14:34:08 +0900

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,37 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+fun getInput(input: List<String>): List<List<Int>> {
+    val a = mutableListOf<Int>()
+    val b = mutableListOf<Int>()
+    input.forEach { s ->
+        val (v1, v2) = s.split("   ").map { it.toInt() }
+        a += v1
+        b += v2
+    }
+
+    return listOf(a, b)
+}
+
+fun part1(input: List<String>): Int =
+    getInput(input).map { it.sorted() }.let { (a, b) ->
+        a.mapIndexed { i, x -> (x - b[i]).absoluteValue }.sum()
+    }
+
+
+fun part2(input: List<String>): Int =
+    getInput(input).let { (a, b) ->
+        a.sumOf { it * b.count { x -> it == x } }
+    }
+
